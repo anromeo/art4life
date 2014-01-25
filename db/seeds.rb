0732a0f4557e979c9266a90e64643268112e80a9
dtortola <dtortola@lockerz.com>
2014-01-25 15:20:21 -0800

    some more sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,10 @@ Keyword.create(name: "glass")
 Keyword.create(name: "wood")
 Keyword.create(name: "steel")
 
-User.create(email: "hey@gmail.com", password: "password")
+User.create(email: "hey_donor@gmail.com", password: "password")
+User.create(email: "hey_artist@gmail.com", password: "password")
+
+Post.create(title: "Spare glass!", description: "Got some spare glass if you need it", address: "207 Boren Ave N", donor_id: 1, keyword: 1)
+Post.create(title: "Wooden table", description: "Old wooden table available!", address: "207 Boren Ave N", donor_id: 1, keyword: 2)
+Post.create(title: "Do you need some steel?", description: "Bought a couple of steel plates that I haven't used for a while", address: "207 Boren Ave N", donor_id: 1, artist_id: 2, keyword: 3)
+
