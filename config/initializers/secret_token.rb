0212071a7fd8c76aa7d028e896b5aa5fa15b737e
anromeo <anthonykzullo@gmail.com>
2014-01-25 10:25:31 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Good4art::Application.config.secret_key_base = 'e22a85da10d64412be78b6f463c16d2677794b55f5a6f814a3c0bf47158e957491d230016d522fa1c117e3229d5c75f2b81ae9e62a79f885ef093879eb4e36b1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Good4art::Application.config.session_store :cookie_store, key: '_good4art_session'
