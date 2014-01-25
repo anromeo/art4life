f8fae9e736ade2f7e64aca43b43c6fed7d8d4fbe
anromeo <anthonykzullo@gmail.com>
2014-01-25 15:32:40 -0800

    changed donor_id

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,4 @@
 class Image < ActiveRecord::Base
   belongs_to :post
-  has_attached_file :url, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :url, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 end
