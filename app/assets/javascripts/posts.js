c3b7ed42ce703a283892e7058b648282ee12efae
dtortola <dtortola@lockerz.com>
2014-01-26 13:50:57 -0800

    geolocation

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -9,4 +9,8 @@ $(document).ready(function() {
       $("#logo").width("190px");
     }
   });
+
+	if (($("h1.subheader")[0].textContent==="New Post") && ($("#post_address")[0].nodeName==="INPUT") && ($("#post_address")[0].value==="")) {
+  	$("#post_address").val(geoip_city()+", "+geoip_region());
+  }
 });
