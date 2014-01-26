399479a8171674ebae415c146df26999b858a38e
anromeo <anthonykzullo@gmail.com>
2014-01-26 15:03:50 -0800

    removed commenting from js

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,15 +1,4 @@
 $(document).ready(function() {
-  # if($(window).width() < 516){
-  #   $("#logo").width("90px");
-  # }
-  # $(window).resize(function(){
-  #   if($(window).width() < 516){
-  #     $("#logo").width("90px");
-  #   }else{
-  #     $("#logo").width("190px");
-  #   }
-  # });
-
 	if (($("h1.subheader")[0].textContent==="New Post") && ($("#post_address")[0].nodeName==="INPUT") && ($("#post_address")[0].value==="")) {
   	$("#post_address").val(geoip_city()+", "+geoip_region());
   }
